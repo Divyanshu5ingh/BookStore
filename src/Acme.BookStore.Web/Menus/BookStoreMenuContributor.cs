2153f7687702ba1f38238a3452ba559eb0530b4d
Divu <d1403kr3@gmail.com>
2023-03-28 17:59:57 +0530

    Step 5

--- src/Acme.BookStore.Web/Menus/BookStoreMenuContributor.cs
+++ src/Acme.BookStore.Web/Menus/BookStoreMenuContributor.cs
@@ -1,6 +1,8 @@
 ﻿using System.Threading.Tasks;
 using Acme.BookStore.Localization;
 using Acme.BookStore.MultiTenancy;
+using Acme.BookStore.Permissions;
+using Volo.Abp.Authorization.Permissions;
 using Volo.Abp.Identity.Web.Navigation;
 using Volo.Abp.SettingManagement.Web.Navigation;
 using Volo.Abp.TenantManagement.Web.Navigation;
@@ -32,7 +34,7 @@ public class BookStoreMenuContributor : IMenuContributor
                 icon: "fas fa-home",
                 order: 0
             )
-        ); 
+        );
         context.Menu.AddItem(
     new ApplicationMenuItem(
         "BooksStore",
@@ -43,9 +45,10 @@ public class BookStoreMenuContributor : IMenuContributor
             "BooksStore.Books",
             l["Menu:Books"],
             url: "/Books"
-        )
+        ).RequirePermissions(BookStorePermissions.Books.Default) // Check the permission!
     )
 );
+
 
         if (MultiTenancyConsts.IsEnabled)
         {
